81796525f74c5c0f2309277cdbabf75b5c2727a4
Christopher <cmunden@alumni.unca.edu>
2024-08-14 04:08:44 -0400

    Added basic front end, Created stubs for GameController and Requests for RESTFUL Api Calls.

---
+++ src/main/kotlin/cmunden/retrogamingwebiste/controllers/GameController.kt
@@ -0,0 +1,10 @@
+package cmunden.retrogamingwebiste.controllers
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GameController {
+    public fun getGames() {
+
+    }
+}
